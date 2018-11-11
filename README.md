fdf00cb846eabb0e9837bcef8883a5e77be12cb5
Nikhil Bhattasali <nikhilxb@stanford.edu>
2018-11-11 15:37:48 -0800

    Sketch out implementation of grammar

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Hierarchical Imitation Learning with a Probabilistic Policy Grammar
 
-We're constructing a novel robotic learning algorithm for the class CS 333 (Safe & Interactive Robotics) that combines learning 
-from demonstration with hierarchical reinforcement learning. The project is currently in progress. 
+We're constructing a novel robotic learning algorithm for the class CS 333 (Safe & Interactive Robotics) that combines learning
+from demonstration with hierarchical reinforcement learning. The project is currently in progress.
 
 ## Authors
-**Nikhil Bhattassali**, Department of Computer Science, Stanford University \
+**Nikhil Bhattasali**, Department of Computer Science, Stanford University \
 **Sean Mullane**, Department of Applied Physics, Stanford University 
---
+++ ppg/grammar.py
@@ -0,0 +1,52 @@
+from typing import List, Tuple, Dict
+import torch
+
+"""
+Policy primitive outputs a softmax over action space
+N_actions --> dimensionality of action space
+
+probs equiv log(probs)
+"""
+
+class PolicyGrammar(object):
+    """This class specifies a probabilistic policy grammar."""
+
+    def __init__(self):
+        self.rules: Dict[Goal, List[Production]] = {}  # {"subgoal": }
+
+    def forward(self, root: Token) -> Tensor[N_actions]:
+        """
+        Forward pass for the PPG DAG; construct the probability distribution
+        P(a | s)
+        """
+        # Base case: Policy primitives return probs over actions.
+        if(root.is_primitive): return root.policy_probs
+
+        # Recursive case:
+        for production in self.rules[root]:
+            # How do we work production and activation probabilities into this?
+            return self.forward
+
+class Token(object):
+    def __init__(self,
+                 activation_prob: Tensor,
+                 is_primitive: bool):
+        self.activation_prob: Tensor = activation_prob
+        self.is_primitive: bool = is_primitive
+
+class Goal(Token):
+    def __init__(self,
+                 name: str):
+        self.name: str = name
+
+class PolicyPrimitive(Token):
+    def __init__(self,
+                 name: str,
+                 action_probs: Tensor):
+        pass
+
+class Production(object):
+    def __init__(self,
+                 tokens: List[Token],
+                 production_prob: Tensor):
+        pass
